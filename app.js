7045bac2b5d2d7ac424f0383ab253b2b545989a6
Becca-Hamzy <oyewoleolufunmi@gmail.com>
2024-08-24 11:22:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,147 @@
+// const b = 48 * 8;
+// console.log(b);
+
+// const a = 64/8;
+// console.log(a);
+
+// const c = 9-1
+// console.log(c);
+
+// const d = 4+4
+// console.log(d);
+
+// let f = 5;
+// let g = 2
+// let e = 3
+// let h = 5 + 2 - 3
+// console.log(h);
+
+// // const x = window.prompt("Enter a value for x:");
+// // const y = window.prompt("Enter the value for y:");
+// // const z = x + y;
+// // console.log(typeof z);
+
+// // i = parseInt(i);
+// // j = parseInt (j);
+// // const k = i + j;
+// // console.log(k);
+
+// let m = "50"
+// let n =  5
+// let o = m * n 
+// console.log(o);
+
+// let r = 5
+// let s = "50"
+// let t = r + s
+// console.log(t);
+
+// const u = 250
+// const v = 25
+// console.log(u > u);
+// console.log(u > v);
+// console.log(u < v);
+// console.log(u == v);
+// console.log(u === v);
+// console.log(u != v);
+// console.log(u >! v);
+// console.log(u <! v);
+// console.log(u >= v);
+// console.log(u <= v);
+
+
+// // If the value of 4 numbers are 100, 200, 200, and 400, find the average using the prompt method.
+
+// let w = 100
+// let x = 200
+// let y = 200
+// let z = 400
+//  let q = (100+200+200+400)/ 2
+//  console.log(q);
+
+// // let w = parseInt (window.prompt ("Enter the value of w"));
+// // let x = parseInt (window.prompt ("Enter the value of x"));
+// // let y = parseInt (window.prompt ("Enter the value of y"));
+// // let z = parseInt (window.prompt ("Enter the value of z"));
+// // const q = (w+x+y+z)/2
+// // console.log(q);
+
+// // And Operator
+// const isUniform = true;
+// const isSchoolfee = true;
+// const isBook = true;
+// console.log(isUniform && isSchoolfee || isBook); 
+
+// // Or Operator
+// const isTextbook = true;
+// const isLaptop = false;
+// const isStationery = true;
+
+// console.log(isTextbook || isLaptop || isStationery);
+
+// // Unary Operator
+// let p = 60;
+// p++;
+// console.log(p);
+
+// // Turnary Operator
+// // condition: true or false
+
+// // Write a JS program that determines if a person is eligible to vote
+
+// // let age = 14
+// // let check = age >= 18 ? "you are eligible to vote" : "Try again Next year";
+// // console.log(check);
+
+// // age plus voterscard
+// // let age = 18;
+// // let voterscard = true;
+
+  
+// // let check = age >= 18 && voterscard ? "You are eligible" : "You are not eligible";
+// // console.log(check);
+
+// // alert
+// // let age = parseInt(window.prompt("Enter Your Age"));
+// // let votersCard = (window.prompt ("Do you have a votersCard?"));
+// // answer = age >= 18 && votersCard ? "you are eligible" : "you are not eligible";
+// // alert(answer);
+
+// // Create a JS that determines the greatest between 2 numbers
+
+// // let Num1 = Number(window.prompt ("Enter your number"));
+// // let Num2 = Number(window.prompt("Ener Your Number"));
+// // ans = Num1 > Num2 ? "Num 1 is greater" : "num 2 is greater"
+// // alert(ans)
+
+// // Create a JS that determines the greatest between 3 numbers
+
+// // let Num1, Num2, Num3, Answer;
+// // let Num1 = Number(window.prompt("Enter the number"));
+// // let Num2 = Number(window.prompt("Enter the number"));
+// // let Num3 = Number(window.prompt("Enter the number"));
+// // Answer =  `${Math.max(Num1, Num2, Num3)} is the greatest number; `
+// // alert(Answer)
+
+// // Create a JS that shows a number is positive or negative
+// Num = Number(window.prompt("Enter The Number"));
+// check = Num >=0 ? "Positive Number" : "Negative Number";
+// alert(check)
+
+
+
+
+
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
